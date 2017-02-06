cbff7aa08d48a23badedfe9816bedab877925ebd
cifong <raprope@gmail.com>
2017-02-06 15:59:11 +0800

    設定檔初始化

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ module.exports = {
 	// 某通账户
 	"accounts": [
 		{
-			"user": 15500000000,
+			"user": 'useracc',
 			"password": 123456
 		}
 	],
 
 	// 執行的網址
 	"url": {
-		"login": 'eip.ho-chu.net/',
-		"sign": 'xxx@163.com'
+		"login": 'login_page',
+		"sign": 'sigin_page'
 	}
 };

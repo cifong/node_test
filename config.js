6501ab6c564da0efa63078881de5afc0345d0b83
cifong <raprope@gmail.com>
2017-02-06 15:06:57 +0800

    nodetest
    自動簽到 測試

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	// 某通账户
+	"accounts": [
+		{
+			"user": 15500000000,
+			"password": 123456
+		}
+	],
+
+	// 執行的網址
+	"url": {
+		"login": 'eip.ho-chu.net/',
+		"sign": 'xxx@163.com'
+	}
+};
---
+++ hello.js
@@ -0,0 +1,6 @@
+var accounts = require('./config').accounts;
+var url = require('./config').url;
+var requestModule = require('superagent');
+console.log(accounts);
+console.log(url);
+console.log("hello!");
